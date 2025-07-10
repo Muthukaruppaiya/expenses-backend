88e265543c75e5fc880a558dde1cf6eb00542144
Muthu karuppaiya <129316955+Muthukaruppaiya@users.noreply.github.com>
2025-07-10 10:48:05 +0530

    Update WebConfig.java

--- src/main/java/com/project/expenses/WebConfig.java
+++ src/main/java/com/project/expenses/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://686e2aecb24b5117eaca1e0e--resilient-parfait-cfd883.netlify.app/")
+                .allowedOrigins(""https://resilient-parfait-cfd883.netlify.app"")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
