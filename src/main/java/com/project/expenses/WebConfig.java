635f34cf8600f0a4af1346b64f5e583f4d5d7d12
Muthu karuppaiya <129316955+Muthukaruppaiya@users.noreply.github.com>
2025-07-10 10:54:10 +0530

    Update WebConfig.java

--- src/main/java/com/project/expenses/WebConfig.java
+++ src/main/java/com/project/expenses/WebConfig.java
@@ -1,6 +1,5 @@
 package com.project.expenses;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(""https://resilient-parfait-cfd883.netlify.app"")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                .allowedOrigins(
+                    "https://resilient-parfait-cfd883.netlify.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
